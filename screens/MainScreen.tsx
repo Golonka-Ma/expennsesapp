59017f7aecec9b22f68531b34073cd616b562a59
Marcin Golonka <BattelShark@gmail.com>
2024-05-07 12:28:45 +0200

    Otworzenie połączenia, pobieranie oraz wyświetlanie elementów z bazy danych

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,27 +1,26 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import { doc, getDoc } from 'firebase/firestore';
+import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import { doc, getDoc, addDoc, collection, onSnapshot, query, where } from 'firebase/firestore';
 import { db } from '../FirebaseConfig';
 import { auth } from '../FirebaseConfig';
 import { useFocusEffect } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-
 const MainScreen: React.FC = () => {
   const [userName, setUserName] = useState<string>('');
   const [isAddingExpense, setIsAddingExpense] = useState<boolean>(false);
   const [itemName, setItemName] = useState<string>('');
   const [itemPrice, setItemPrice] = useState<string>('');
+  const [expenses, setExpenses] = useState<any[]>([]);
   const navigation = useNavigation();
 
   const fetchUserName = useCallback(async () => {
     try {
       const currentUser = auth.currentUser;
-      console.log(currentUser);
       if (currentUser) {
         const userUID = currentUser.uid;
-        const docRef = doc(db, 'users', userUID); 
+        const docRef = doc(db, 'users', userUID);
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
@@ -32,7 +31,7 @@ const MainScreen: React.FC = () => {
         }
       } else {
         console.log('User not logged in!');
-        navigation.navigate('Login'); // Przekieruj użytkownika do ekranu logowania
+        navigation.navigate('Login');
       }
     } catch (error) {
       console.error('Error fetching user data:', error);
@@ -42,32 +41,66 @@ const MainScreen: React.FC = () => {
   useFocusEffect(
     useCallback(() => {
       fetchUserName();
+      // Subskrybujemy na zmiany w kolekcji "wydatki" i aktualizujemy stan expenses
+      const unsubscribe = onSnapshot(
+        query(collection(db, 'wydatki'), where('uid', '==', auth.currentUser.uid)),
+        (snapshot) => {
+          const expensesData: any[] = [];
+          snapshot.forEach((doc) => {
+            expensesData.push({ id: doc.id, ...doc.data() });
+          });
+          setExpenses(expensesData);
+        }
+      );
+      // Czyszczenie subskrypcji
+      return () => unsubscribe();
     }, [fetchUserName])
   );
 
   const handleAddExpense = () => {
-    setIsAddingExpense(true); // Ustawiamy stan na true, aby wyświetlić okienko dodawania wydatku
+    setIsAddingExpense(true);
   };
 
   const handleCancelAddExpense = () => {
-    setIsAddingExpense(false); // Ustawiamy stan na false, aby ukryć okienko dodawania wydatku
-    setItemName(''); // Czyścimy stan nazwy przedmiotu
-    setItemPrice(''); // Czyścimy stan ceny przedmiotu
+    setIsAddingExpense(false);
+    setItemName('');
+    setItemPrice('');
   };
 
-  const handleSaveExpense = () => {
-    // Tutaj możesz zapisać nowy wydatek do bazy danych lub wykonać inne operacje
-    console.log('Dodano nowy wydatek:', itemName, itemPrice);
-    handleCancelAddExpense(); // Po dodaniu wydatku, chcemy ukryć okienko dodawania wydatku i wyczyścić pola
+  const handleSaveExpense = async () => {
+    try {
+      // Dodaj nowy wydatek do kolekcji "wydatki" z nazwą dokumentu UID
+      await addDoc(collection(db, 'wydatki'), {
+        uid: auth.currentUser.uid, // Dodajemy UID użytkownika jako pole w dokumentcie
+        nazwa: itemName,
+        cena: itemPrice,
+        data: new Date().toISOString() // Możesz dodać również datę, na przykład datę dodania wydatku
+      });
+
+      handleCancelAddExpense(); // Po dodaniu wydatku, chcemy ukryć okienko dodawania wydatku i wyczyścić pola
+    } catch (error) {
+      console.error('Błąd podczas dodawania wydatku:', error);
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-  <Text style={styles.title}>Witaj {userName} na ekranie głównym!</Text>
-  <TouchableOpacity style={styles.addButton} onPress={handleAddExpense}>
-    <FontAwesome name="plus" size={24} color="white" />
-  </TouchableOpacity>
+        <Text style={styles.title}>Witaj {userName} na ekranie głównym!</Text>
+        <TouchableOpacity style={styles.addButton} onPress={handleAddExpense}>
+          <FontAwesome name="plus" size={24} color="white" />
+        </TouchableOpacity>
+        {/* Lista wydatków */}
+        <FlatList
+          data={expenses}
+          renderItem={({ item }) => (
+            <View style={styles.expenseItem}>
+              <Text>Nazwa: {item.nazwa}</Text>
+              <Text>Cena: {item.cena}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
         {/* Okienko dodawania wydatku */}
         {isAddingExpense && (
           <View style={styles.addExpenseContainer}>
@@ -85,10 +118,10 @@ const MainScreen: React.FC = () => {
             />
             <View style={styles.buttonsContainer}>
               <TouchableOpacity style={styles.cancelButton} onPress={handleCancelAddExpense}>
-                <Text>Anuluj</Text>
+                <Text style={styles.Buttontext}>Anuluj</Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.saveButton} onPress={handleSaveExpense}>
-                <Text>Zapisz</Text>
+                <Text style={styles.Buttontext}>Zapisz</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -115,10 +148,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 20,
     right: 20,
-    backgroundColor: 'blue',
+    backgroundColor: '#800000',
     borderRadius: 50,
-    width: 50,
-    height: 50,
+    width: 60,
+    height: 60,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -154,6 +187,17 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: 5,
     alignItems: 'center',
+  },
+  Buttontext: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  expenseItem: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
   },
 });
 
